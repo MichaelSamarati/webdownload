f306fdd5f02772deec291100049f1d37bd008aac
MichaelSamarati <87271458+MichaelSamarati@users.noreply.github.com>
2022-06-21 15:37:33 +0200

    Communication

--- api/server.js
+++ api/server.js
@@ -15,8 +15,6 @@ import JSZip from 'jszip';
 import fs from 'fs';
 import extractUrls from 'extract-urls';
 import { Server } from 'socket.io';
-//import ss from 'socket.io-stream'
-//import { Stream } from 'stream';
 
 const io = new Server({
     cors: {
@@ -25,24 +23,34 @@ const io = new Server({
   });
   
   io.on("connection", socket => {
-    console.log("Connected with socket id: "+socket.id);
-
-    // socket.on('webdownload', function () {
-    //   var stream = ss.createStream();
-    //   stream.on('end', function () {
-    //       console.log('file sent');
-    //   });
-    //   ss(socket).emit('zip', stream); 
-    //   fs.createReadStream("tile.zip").pipe(stream);
-    // });  
-
-    socket.on("webdownload",  (arg) => {
+    console.log("Connected with socket id: "+socket.id); 
+    socket.on("webdownload", async (arg) => {
         console.log("link"+arg);
 
         const zip = new JSZip();
         zip.file("test.txt", "Just to see if zip works");
-        zip.generateAsync({ type: "base64", compression: 'DEFLATE' });
-        //fs.writeFile('test10.zip', zip.generate({ type: "nodebuffer" }), 'binary', function (error) {});
+        zip.generateAsync({ type: "nodebuffer" });
+
+        socket.emit("zip", "Path", "Test")
+        console.log(fs.readFileSync("towplane.jpg").toString('base64').length+ " dwa da dw")
+
+        fs.readFile('ms.png', function(err, data){
+          socket.emit('png', "data:image/png;base64,"+ data.toString("base64"));
+        });        
+        fs.readFile('towplane.jpg', function(err, data){
+          socket.emit('jpg', "data:image/jpg;base64,"+ data.toString("base64"));
+        });
+      
+    });
+  });
+
+  
+  io.listen(3080);
+
+  
+//https://gist.github.com/companje/b95e735650f1cd2e2a41
+/*
+        //fs.writeFile('test10.zip', zip.generate({ type: "base64" }), 'binary', function (error) {});
         //     console.log('wrote test1.zip', error);
         // });
         //var bufArr = new ArrayBuffer(zip);
@@ -51,18 +59,28 @@ const io = new Server({
         //stream.pipe(fs.createWriteStream('test10.zip'));
         //socket.emit("zip", {'file':zip.toString('base64')});
         //socket.emit("zip", fs.createReadStream('file.jpg'))
-        socket.emit("zip", {'file':zip.toString('base64')});
+
+ //socket.emit("png", fs.readFileSync("ms.png", {encoding: 'base64'})) // , { image: true, buffer: data }
+
+        //socket.emit("jpg", fs.readFileSync('towplane.jpg', {encoding: 'base64'})) // , { image: true, buffer: data }
+        
+
+        //socket.emit("jpg", { image: true, buffer: (fs.readFileSync('towplane.jpg', 'base64').toString())})
+        //const buf = fs.readFileSync("towplane.jpg");
+        //socket.emit("jpg", "Path", { image: true, buffer: buf });
+
+
+        //socket.emit("zip", {'file':fs.readFile("test10.zip", 'utf8').toString('base64')});
+        //const bytes = new Uint8Array(zip);
+        //console.log(zip)
+        //socket.emit("zip", new Blob(["awdwaawd"]));
         //socket.emit(fs.createReadStream(zip.generate({ type: "nodebuffer" })));
+*/ 
 
-    });
-    socket.on("status",  () => {
-        console.log("status");
 
-    });
-  });
-  io.listen(3080);
 
-  
+
+
 
 // var app = express();
 // var port = 3080;
