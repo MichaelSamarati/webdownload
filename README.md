69ac767f99b5d7fdaf9e54c758e09fda50a3c0af
MichaelSamarati <87271458+MichaelSamarati@users.noreply.github.com>
2022-06-21 15:57:54 +0200

    Saving Comments

--- README.md
+++ README.md
@@ -1,22 +1,21 @@
 # WebDownload
 
 ## Usage
-
 WebDownload is an JavaScript app to download a website for offline usage. 
-Disclaimer! It does only provide a static version of a website. It is not able to download the code of websites.
+
+### Disclaimer! 
+It does only provide a static version of a website. It is not able to download the code of websites.
 
 ## Example
 
 
 
 ## Dependencies
-
 - Client
   - react
   - react-bootstrap
   - socket.io-client
   - ...
-
 - Server
   - socket.io
   - jszip
--- app/src/logic.js
+++ app/src/logic.js
@@ -7,34 +7,40 @@ import io from 'socket.io-client';
 //import ss from 'socket.io-stream';
 //import fs from 'fs';
 
-const socket = io("http://localhost:3080");
+/*
 
-socket.on('connect', () => {
-    console.log(socket.id);
-    socket.emit("webdownload", "Test123");
-})
+            //console.log(buffer.length+" awdw ads");
+            //onsole.log("new Blob(buffer), {type: application/zip}");
+            //const bytes = Buffer.from(buffer);
+function b64(e){var t="";var n=new Uint8Array(e);var r=n.byteLength;for(var i=0;i<r;i++){t+=String.fromCharCode(n[i])}return window.btoa(t)}
+            //console.log(buffer.length+" awdw ads");
+            //onsole.log("new Blob(buffer), {type: application/zip}");
+            //const bytes = Buffer.from(buffer);
+            //const reader = new FileReader();
 
-socket.on("zip", function(buffer){
-    console.log(buffer);
-    console.log("new Blob(buffer), {type: application/zip}");
-    const blob = new Blob([buffer], {type: "application/zip"});
-    saveAs(blob, "new.zip");
-    
- } )
-//  ss(socket).on('zip', function(stream) {
-//     stream.pipe(fs.createWriteStream(filename)); 
-//     stream.on('end', function (file) {
-//       console.log('file received');
-//       saveAs(file, "new.zip");
-//     });
-//   });
+                        // const blob = await new Blob([msg], {type: "image/jpeg;base64"});
+            //const blob = reader.readAsDataURL(new Blob([msg], {type: "image/jpeg;base64"}));
 
-export default async function downloadWebpage(name, link, iterations){
-    console.log("Download started!")
-    try{
-        socket.emit("webdownload", link);
 
-        
+            //console.log(buffer.length+" awdw ads");
+            //onsole.log("new Blob(buffer), {type: application/zip}");
+            //const bytes = Buffer.from(buffer);
+
+                        //const blob = await new Blob([msg], {type: "image/png;base64"});
+            //const reader = new FileReader();
+            //const blob = reader.readAsDataURL(new Blob([msg], {type: "image/png;base64"}));
+
+
+        //  ss(socket).on('zip', function(stream) {
+        //     stream.pipe(fs.createWriteStream(filename)); 
+        //     stream.on('end', function (file) {
+        //       console.log('file received');
+        //       saveAs(file, "new.zip");
+        //     });
+        //   });
+
+
+                
         // console.log("awdwad")
         // console.log(link)
         // const response = await axios.get("http://localhost:3080/webdownload", { 
@@ -49,8 +55,42 @@ export default async function downloadWebpage(name, link, iterations){
         //     console.log('Looks like there was a problem. Status Code: ' + response.status);
         //     return;
         // }
+*/
+
+
+export default async function downloadWebpage(name, link, iterations) {
+    console.log("Download started!")
+    try {
+        const socket = io("http://localhost:3080");
+
+        socket.on('connect', () => {
+            console.log(socket.id);
+            socket.emit("webdownload", "Test123");
+        })
+
+        socket.on("text", async function (path, msg) {
+            const blob = new Blob([msg], { type: "text/plain" });
+            console.log(blob.size + "  text")
+            saveAs(blob, "new.txt");
+
+        })
+        socket.on("jpg", async function (msg) {
+            const blob = new Blob([msg]);
+            console.log(blob.size + "  jpg")
+            saveAs(msg, "towplane.jpg");
+
+        })
+        socket.on("png", async function (msg) {
+            const blob = new Blob([msg]);
+            console.log(blob.size + "  png")
+            saveAs(msg, "ms.png");
+
+        })
+
+        socket.emit("webdownload", link);
+
         console.log("Download finished!")
-    }catch(e){
+    } catch (e) {
         console.log(e)
         console.log("Download failed!")
     }
